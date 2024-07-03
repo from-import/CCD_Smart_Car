72c0b088af3b0749ca9980a06d552e08b20e1932
import_urllib7 <132730866+from-import@users.noreply.github.com>
2024-07-03 07:01:14 -0700

    修正了入环判断算法，添加了入环前半段的补线操作

--- Find_Circle.py
+++ Find_Circle.py
@@ -28,23 +28,20 @@ def is_circle(ccd_data):
         return False, "nothing"
 
     # 检查左环的条件
-    left_ring = False
-    for i in range(0, 30):
+    for i in range(0, 20):
         if ccd_data[i:i + 5] == [1, 1, 1, 1, 1]:
-            left_ring = True
             return True, "left"
 
     # 检查右环的条件
-    right_ring = False
-    for i in range(100, 127):
+    for i in range(105, 127):
         if ccd_data[i:i - 5:-1] == [1, 1, 1, 1, 1]:
-            right_ring = True
             return True, "right"
+    return False, "nothing"
 
 
 """
 函数名: Go_circle_now
-作用: 通过分析CCD数据和道路宽度变化，判断是否现在进行舵机打角入环。
+作用: 通过分析CCD数据和道路宽度变化，判断是否现在检测到了环的中点位置
 
 参数:
 ccd_data (list): 包含二值化后的CCD数据，其中1表示道路，0表示非道路。
@@ -67,11 +64,56 @@ def Go_circle_now(ccd_data, last_width=0):
 
     # 判断中线是否在 64 ± 5 范围内
     if abs(mid_line - 64) <= 5:
+
         # 判断宽度是否变小,来判断CCD是否扫描到了环的中点位置
         if last_width > current_width:
             return True
 
     return False
+
+
+"""
+函数名: fill_line
+作用: 根据中线位置对CCD数据进行对称补线
+
+参数:
+ccd_data (list): 包含二值化后的CCD数据，其中1表示道路，0表示非道路。
+direction (str): 方向，'left' 表示从中线向右查找右边界并补左边界，'right' 表示从中线向左查找左边界并补右边界。
+fill (bool): 是否进行补线操作，True 表示进行补线，False 表示不进行补线。
+lastMiddlePosition (int, 可选): 上一次的中线位置，默认为64。
+
+返回值: int: 中线位置。
+
+说明:
+此函数首先判断是否需要进行补线操作，如果不需要直接返回上一次的中线位置。
+如果需要补线，通过调用 `find_road_edges` 函数来找到道路的左右边界和中线位置。
+根据方向 'left' 或 'right'，分别从中线位置向对应方向查找边界。
+找到边界后，在边界位置±21处进行对称补线操作。
+最终返回修改后的中线位置。
+"""
+
+
+def fill_line(ccd_data, direction, fill, lastMiddlePosition=64):
+    if not fill:
+        return lastMiddlePosition
+
+    left_edge, right_edge, mid_line = find_road_edges(ccd_data, lastMiddlePosition)
+
+    if direction == 'left':
+        # 左环，优先向右查找边界
+        if right_edge != lastMiddlePosition:
+            mid_line = right_edge - 20
+        else:
+            mid_line = lastMiddlePosition
+
+    elif direction == 'right':
+        # 右环，优先向左查找边界
+        if left_edge != lastMiddlePosition:
+            mid_line = left_edge + 20
+        else:
+            mid_line = lastMiddlePosition
+
+    return mid_line
 
 
 
